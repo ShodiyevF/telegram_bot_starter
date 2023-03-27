8af7e6d88c327014b703f993c7567a43a93561d2
ShodiyevF <shodiyevf@yandex.ru>
2023-03-27 05:44:06 +0500

    add users crud

--- src/database/model.sql
+++ src/database/model.sql
@@ -1,8 +1,12 @@
-create database users;
+create database botdatabase;
 
-create table users(
+drop table if exists users cascade;
+create table if not exists users(
     user_id int generated always as identity primary key,
-    user_fullname varchar(64) not null,
-    user_username varchar(64) not null unique,
-    user_password varchar(24) not null
+    user_chatid varchar(20) not null,
+    user_username varchar(32) not null unique,
+    user_firstname varchar(64) not null,
+    user_lastname varchar(64),
+    user_isdeleted boolean default false,
+    user_createdat TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- src/modules/initializer.js
+++ src/modules/initializer.js
@@ -1,6 +1,18 @@
 const { start } = require("./starter");
 
 function botModulesInitializer(bot) {
+    bot.on('message', async (msg) => {
+        const chatID = msg.chat.id
+        const user = await uniqRow('select * from users where user_chatid = $1', chatID)
+        if (!user.rows.length) {
+            const query = 'insert into users (user_chatid, user_username, user_firstname) values ($1,$2,$3)'
+            await uniqRow(query, msg.chat.id, msg.chat.username, msg.chat.first_name)
+        } else {
+            const query = 'update users set user_username = $1, user_firstname = $2, user_lastname = $3 where user_id = $4'
+            await uniqRow(query, msg.chat.username, msg.chat.first_name, msg.chat.last_name ? msg.chat.last_name : null, user.rows[0].user_id)
+        }
+    })
+    
     bot.onText(/\/start/, (msg) => start(bot, msg))
 }
 
